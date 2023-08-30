13377a5daf805dc798a4c00bde6ebbf88bcb043a
tn7git <90912178+tn7git@users.noreply.github.com>
2023-08-30 20:52:09 +1000

    deploy

---
+++ astro.config.mjs
@@ -0,0 +1,15 @@
+import { defineConfig } from 'astro/config';
+
+import vercel from "@astrojs/vercel/serverless";
+
+// https://astro.build/config
+export default defineConfig({
+  // site: 'https://tn7git.github.io',
+  // base: '/portfolio',
+  experimental: {
+   viewTransitions: true 
+  },
+  output: "server",
+  adapter: vercel()
+  
+});
